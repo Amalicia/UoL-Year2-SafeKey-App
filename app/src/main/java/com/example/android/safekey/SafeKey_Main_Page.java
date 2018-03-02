727e5eaca98f32e2e9bdaab9785c76c21ef8a7cd
Charlotte Bryant <amaliciabryant@gmail.com>
2018-03-02 12:05:14 +0000

    Base
    Basic mainframe for app

---
+++ app/src/main/java/com/example/android/safekey/SafeKey_Main_Page.java
@@ -0,0 +1,37 @@
+package com.example.android.safekey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SafeKey_Main_Page extends AppCompatActivity {
+
+    private Button doorButton;w
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safe_key__main__page);
+
+        doorButton = (Button) findViewById(R.id.doorButton);
+        doorButton.setText(R.string.unlock);
+    }
+
+    public void onClickBtn(View view) {
+        //Toast.makeText(this, "Button pressed", Toast.LENGTH_LONG).show();
+
+        String btnText = doorButton.getText().toString();
+
+        if (btnText.equals("Unlock")) {
+            doorButton.setText(R.string.lock);
+        }
+        else if (btnText.equals("Lock")){
+            doorButton.setText(R.string.unlock);
+        }
+        else {
+            doorButton.setText("Error");
+        }
+    }
+}
