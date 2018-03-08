ac2455e469583df6ab9c7e7f4c1e01058f41a61d
awalford16 <34867732+awalford16@users.noreply.github.com>
2018-03-08 13:02:35 +0000

    Add menu button actions

--- app/src/main/java/com/example/android/safekey/SafeKey_Main_Page.java
+++ app/src/main/java/com/example/android/safekey/SafeKey_Main_Page.java
@@ -9,14 +9,36 @@ import android.widget.Toast;
 public class SafeKey_Main_Page extends AppCompatActivity {
 
     private Button doorButton;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safe_key__main__page);
 
+        //Menu button setup
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
         doorButton = (Button) findViewById(R.id.doorButton);
         doorButton.setText(R.string.unlock);
+    }
+    
+    //Menu button action on click
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void onClickBtn(View view) {
